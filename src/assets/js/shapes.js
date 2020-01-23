ef83c42ef80d39cec32f483db5bd34072b68ebc4
dwilliams64 <dwilliams_64@yahoo.com>
2020-01-23 09:33:27 -0600

    Explored SVG basics

--- src/assets/js/shapes.js
+++ src/assets/js/shapes.js
@@ -1,18 +1,3 @@
 var d3 = require("d3");
 
-let dataArray = [5, 11, 18];
 
-let svg = d3.select('body').append('svg').attr('height', '100%').attr('width', '100%');
-
-// selectAll() works the same way as the DOM version.
-
-// selectAll() will look inside of element and search for what we put inside of the argument.
-
-// Since we are searching for a retangle ('rect') and there are not any rectangles inside of
-// our svg element an empty selection will be returned.
-
-
-svg.selectAll('rect')
-    .data(dataArray)
-    .enter()
-    .append('rect');
--- tracking.md
+++ tracking.md
@@ -1,1 +1,5 @@
-Down 1 D3
+Down 1 D3 
+
+datavis-2018 
+
+DCS, GoodCore Software
