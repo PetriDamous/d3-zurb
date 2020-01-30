32875e5a3c60878bb420741663c35dc5a6c27541
dwilliams64 <dwilliams_64@yahoo.com>
2020-01-30 09:09:45 -0600

    Created simple bar chart

--- src/assets/js/app/datavis-2018/bar-chart.js
+++ src/assets/js/app/datavis-2018/bar-chart.js
@@ -1,9 +1,13 @@
-import { select, csv, scaleLinear, max } from 'd3';
+import { select, csv, scaleLinear, max, scaleBand, axisLeft, axisBottom } from 'd3';
 
 const svg = select('svg');
 
 const svgWidth = +svg.attr('width');
 const svgHeight = +svg.attr('height');
+
+const elmFill = 'steelblue';
+
+const fontSize = '1rem';
 
 // We are using our render() function to create a data join.
 
@@ -13,6 +17,12 @@ const svgHeight = +svg.attr('height');
 
 // data() The data() function is used to join the specified array of data to the selected DOM elements and 
 // return the updated selection. D3 works with different types of data like Array, CSV, TSV, JSON, XML etc.
+
+// data() will join data to elements in order. If we are drawing 3 rects and our data set looked like [5, 10, 25]
+// then d3 would map 5 to the 1st rect, 10 to the second rect, and 25 to the 3rd rect.
+
+// If we only had 2 rects on the stage then d3 will map 5 to the 1st rect, 10 to the 2nd rect, and place 25 in
+// an enter selection if .enter() method is present.
 
 // The enter() method dynamically creates placeholder references corresponding to the number of data values. The output of enter() can be fed to append() 
 // method and append() will create DOM elements for which there are no corresponding DOM elements on the page.
@@ -41,19 +51,122 @@ const svgHeight = +svg.attr('height');
 
 // Think of it as a way to constrain the physical size of the chart on our screen.
 
-// For example if our .range([50, 500]) with a .domain([100, 1000]) 
+// For example if our .range([50, 500]) with a .domain([100, 1000]) 100 would map to 50 and 1000 would map to 500.
+
+// domain is the data space.
+
+// range is the screen space.
+
+/*
+
+Band Scales
+
+Band scales are like ordinal scales except the output range is continuous and numeric. Discrete output values are automatically 
+computed by the scale by dividing the continuous range into uniform bands. Band scales are typically used for bar charts with 
+an ordinal or categorical dimension. The unknown value of a band scale is effectively undefined: they do not allow implicit domain construction.
+
+scaleBand() Constructs a new band scale with the specified domain and range, no padding, no rounding and center 
+alignment. If domain is not specified, it defaults to the empty domain. If range is not specified, 
+it defaults to the unit range [0, 1].
+
+Band scales are useful for oridinal attributes.
+
+https://github.com/d3/d3-scale/blob/v2.2.2/README.md#scaleBand
+
+*/
+
+
 
 const render = data => {
-    const xScale = scaleLinear()
-        .domain([0, max(data, d => d.population)])
+    const xValue = d => d.population;
+    const yValue = d => d.country;
+
+    const margin = {top: 20, right: 50, bottom: 20, left: 120};
+
+    const innerHeight = svgHeight - margin.top - margin.bottom;
+    const innerWidth = svgWidth - margin.right - margin.left;
+
+    // Note: d represents one row in our data table.
+    const xScale = scaleLinear()        
+        .domain([0, max(data, xValue)])
+        .range([0, innerWidth]);    
+    
         
-        console.log(xScale)
+    const yScale = scaleBand()
 
+        // Sets our domain to the country values
+        .domain(data.map(d => yValue(d)))
 
-    svg.selectAll('rect').data(data)
+        // Arranges data elements (rects/bars) to go from top to bottom.
+        .range([0, innerHeight])
+
+        // Adds padding between elements.
+        // Argument takes a numeric value that represents the pixils to pad by.
+        .padding(0.1)
+         
+        
+    const barG = svg.append('g')
+        .attr('transform', `translate(${margin.left}, ${margin.top})`);
+
+    /*
+
+    axisLeft() Constructs a new left-oriented axis generator for the given scale, with empty tick arguments, 
+    a tick size of 6 and padding of 3. In this orientation, ticks are drawn to the left of the vertical domain path.
+
+    axisBottom() Constructs a new bottom-oriented axis generator for the given scale, with empty tick arguments, 
+    a tick size of 6 and padding of 3. In this orientation, ticks are drawn below the horizontal domain path.
+
+    https://github.com/d3/d3-axis/blob/v1.0.12/README.md#axis_scale
+
+    https://www.tutorialspoint.com/d3js/d3js_axis_api.htm
+
+    https://www.tutorialsteacher.com/d3js/axes-in-d3
+    
+    */
+
+    const yAxis = axisLeft(yScale);
+
+    const xAxis = axisBottom(xScale);
+
+    /*
+    
+    selection.call() Invokes the specified function exactly once, passing in this selection along with any optional arguments. Returns this selection. 
+    This is equivalent to invoking the function by hand but facilitates method chaining.
+    
+    We can call a function on a d3 selected element.
+
+    */
+    barG.append('g').call(yAxis);
+
+    barG.append('g').call(xAxis)
+        .attr('transform', `translate(0, ${innerHeight})`);
+
+    // First we select all the rect elements which there are not any currently.
+
+    // Then we bind the data to the rect elements using the data() method
+    // and passing in our data as an argument.
+    
+    barG.selectAll('rect').data(data)
+        // Since there are no rects inside the svg currently we store the data using the .enter() method
+        // We then take the stored data and append them to rects
         .enter().append('rect')
-            .attr('width', 300)
-            .attr('height', 300)
+            .attr('fill', elmFill)
+            // Calculates the height of each bar but since these bars are side ways it is used to calculate how long they
+            // or the width of the bar.            
+            .attr('width', d => xScale(xValue(d)))
+
+            // Will seperate our bars/rects along the y-axis based on the number of countries in our data.
+            .attr('y', d => yScale(yValue(d)))
+            // bandwidth is the computed value of a single bar.
+
+            // band.bandwith() Returns the width of each band.
+            // https://github.com/d3/d3-scale/blob/v2.2.2/README.md#band_bandwidth
+            // Since our bar chart moves from left to right rather than top to bottom
+            // we will be using bandwidth to calculate the height.
+            
+            // This code controls how wide the bars are but in this case the height of each bar since they are sitting side ways.
+            .attr('height', yScale.bandwidth())
+                
 };
 
 // The csv() function makes a HTTP request and 
